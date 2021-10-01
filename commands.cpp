ba20a8ec5e2d83102f7997e7d07fac87f3161186
NickPreuchil <73394792+NickPreuchil@users.noreply.github.com>
2021-10-01 15:54:46 +0300

    Add files via upload

---
+++ commands.cpp
@@ -0,0 +1,353 @@
+﻿/*****************************************************************//**
+ * \file  commands.cpp
+ * \brief Файл реализации функций, выполняющих операции над изображениями.
+ * \author Преучиль Н.А.
+ * \date   12 January 2021
+ *********************************************************************/
+#include "commands.h"
+
+
+/**
+ * \brief Поворот изображений.
+ * \details В зависимости от переданного аргумента, функция может повернуть изображение на 0, 90, 180, 270 и 360 градусов по часовой стрелке.
+ * \details Функция также вносит изменения в структуру изображения, поскольку тот факт, что мы меняем местами
+ * ширину и высоту изображения, может повлиять на то количество байтов, которое нужно добавить к каждой линии развёртки до длины кратной 4.
+ * \details Алгоритм реализуется путём прохода по массиву пикселей старого и нового изображения по разным траекториям (подробнее в коде программы).
+ * \details Этапы работы функции:
+ * - выделение памяти для нового массива пикселей.
+ * - выбор режима работы в зависимости от переданного аргумента.
+ * - перезапись пикселей.
+ * - при повороте на 90 и 270 необходимо учесть изменение разрешения (подробнее выше)
+ * - освобождение выделенной ранее памяти для старого массива пикселей.
+ * - передача новой ссылки на массив пикселей в структуру изображения.
+ * \param Pic* Picture - указатель на структуру изображения, которое нужно повернуть.
+ * \param char* Arg - строка с аргументом функции (числовое значение ANGLE ε {0, 90, 180, 270, 360})
+ * \return void.
+ */
+void rotate(Pic* Picture, char* Arg){
+	int Angle = atoi(Arg);									// Получение численного значения строки - аргумента
+
+	// Выделение памяти для нового массива пикселей.
+	Line* ptr_linTemp;
+	ptr_linTemp = new Line[1000];
+	// Выбор режима работы в зависимости от переданного аргумента.
+	switch (Angle) {
+		case 180: {
+			// Перезапись пикселей.
+			// Строки и пиксели в каждой строке записываются в обратном порядке. 
+			for (int i = 0; i < Picture->Info.biHeight; i++) {
+				for (int j = 0; j < Picture->Info.biWidth; j++) {
+					ptr_linTemp[i].ColourIndex[j] = Picture->ptr_lin[Picture->Info.biHeight - 1 - i].ColourIndex[Picture->Info.biWidth - 1 - j];
+				}
+			}
+			// Освобождение выделенной ранее памяти для старого массива пикселей.
+			delete[] Picture->ptr_lin;
+			// Передача новой ссылки на массив пикселей в структуру изображения.
+			Picture->ptr_lin = ptr_linTemp;
+			break;
+		}
+		case 90: {
+			// Перезапись пикселей.
+			// Элементы строк изначального массива с индексом (Ширина рисунка - i) становятся i-ми строками нового массива.
+			for (int i = 0; i < Picture->Info.biWidth; i++) {
+				for (int j = 0; j < Picture->Info.biHeight; j++) {
+					ptr_linTemp[i].ColourIndex[j] = Picture->ptr_lin[j].ColourIndex[Picture->Info.biWidth - 1 - i];
+				}
+			}
+			// Изменение структуры изображения 
+			// 1. Высота станвится шириной и наоборот.
+			int Temp;
+			Temp = Picture->Info.biWidth;
+			Picture->Info.biWidth = Picture->Info.biHeight;
+			Picture->Info.biHeight = Temp;
+			// 2. Рассчёт количества доп. байтов.
+			Picture->PadBytes = (4 - (Picture->Info.biWidth % 4));
+			if (Picture->PadBytes == 4)
+				Picture->PadBytes = 0;
+			// 3. Нахождения объёма массива пикселей и общего объёма занимаемого файлом (в байтах)
+			Picture->Info.biSizeImg = Picture->Info.biWidth * Picture->Info.biHeight + Picture->PadBytes * Picture->Info.biHeight;
+			Picture->File.bfSize = Picture->Info.biSizeImg + Picture->File.bfOffBit;
+			// Освобождение выделенной ранее памяти для старого массива пикселей.
+			delete[] Picture->ptr_lin;
+			// Передача новой ссылки на массив пикселей в структуру изображения.
+			Picture->ptr_lin = ptr_linTemp;
+			break;
+		}
+		case 270: {
+			// Перезапись пикселей.
+			// i-е элементы строк изначального массива становятся i-ми строками нового массива.
+			for (int i = 0; i < Picture->Info.biWidth; i++) {
+				for (int j = 0; j < Picture->Info.biHeight; j++) {
+					ptr_linTemp[i].ColourIndex[j] = Picture->ptr_lin[Picture->Info.biHeight - 1 - j].ColourIndex[i];
+				}
+			}
+			// Изменение структуры изображения 
+			// 1. Высота становится шириной и наоборот.
+			int Temp;
+			Temp = Picture->Info.biWidth;
+			Picture->Info.biWidth = Picture->Info.biHeight;
+			Picture->Info.biHeight = Temp;
+			// 2. Рассчёт количества доп. байтов.
+			Picture->PadBytes = (4 - (Picture->Info.biWidth % 4));
+			if (Picture->PadBytes == 4)
+				Picture->PadBytes = 0;
+			// 3. Нахождения объёма массива пикселей и общего объёма занимаемого файлом (в байтах)
+			Picture->Info.biSizeImg = Picture->Info.biWidth * Picture->Info.biHeight + Picture->PadBytes * Picture->Info.biHeight;
+			Picture->File.bfSize = Picture->Info.biSizeImg + Picture->File.bfOffBit;
+			// Освобождение выделенной ранее памяти для старого массива пикселей.
+			delete[] Picture->ptr_lin;
+			// Передача новой ссылки на массив пикселей в структуру изображения.
+			Picture->ptr_lin = ptr_linTemp;
+			break;
+		}
+		case 360:{
+			// С изображением ничего не происходит.
+			// Очищаем выделенную память за ненадобностью.
+			delete[] ptr_linTemp;
+			break;
+		}
+		case 0: {
+			// С изображением ничего не происходит.
+			// Очищаем выделенную память за ненадобностью.
+			delete[] ptr_linTemp;
+			break;
+		}
+		default: {
+			assert(false);														// Остановка программы в случае, если switch не обработал аргумент.
+		}
+	}
+	
+	
+	return;
+}
+
+/**
+ * \brief Изменение яркости пикселей.
+ * \details Функция изменяет яркость изображения с помощю побитового сдвига численного значения каждого пикселя. 
+ * \details Биты, вышедшие за границу однобайтового индекса пикселя отбрасываются.
+ * \details Работа функции заключается в проходе по каждому элементу массива и операции побайтого сдвига на f разрядов влево или вправо в зависимости от знака f:
+ * - при f > 0 - сдвиг на f разрядов влево.
+ * - при f < 0 - сдвиг на f разрядов вправо.
+ * \param Pic* Picture - указатель на структуру изображения, яркость которого нужно изменить.
+ * \param char* Arg - строка с аргументом функции (-8 ≤ f ≤ 8).
+ * \return void.
+ */
+void bright(Pic* Picture, char* Arg) {
+	int f = atoi(Arg);						// Получение численного значения аргумента.
+	// Простой проход по массиву с помошью вложенного цикла.
+	if (f > 0) {
+		for (int i = 0; i < Picture->Info.biHeight; i++) {
+			for (int j = 0; j < Picture->Info.biWidth; j++) {
+				// Операция сдвига.
+				Picture->ptr_lin[i].ColourIndex[j] = Picture->ptr_lin[i].ColourIndex[j] << f;
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < Picture->Info.biHeight; i++) {
+			for (int j = 0; j < Picture->Info.biWidth; j++) {
+				// Операция сдвига.
+				Picture->ptr_lin[i].ColourIndex[j] = Picture->ptr_lin[i].ColourIndex[j] >> -f;
+			}
+		}
+	}
+	return;
+}
+
+/**
+ * \brief Зеркальное отражение изображение.
+ * \details Функция отражает изображение относительно вертикальной или горизонтальной оси, в зависимости от переданного аргумента:
+ * \details Работа алогритма заключается в создании и заполнении нового массива пикселями из старого, с помошью прохода по старому массиву в другом порядке, нежели при заполнении.
+ * \details Этапы работы функции:
+ * - выделение памяти для нового массива пикселей.
+ * - выбор, относительно какой оси будет происходить отражение.
+ * - перезапись пикселей. 
+ * - освобождение выделенной ранее памяти для старого массива пикселей.
+ * - передача новой ссылки на массив пикселей в структуру изображения.
+ * \param Pic* Picture - указатель на структуру изображения, которое нужно отразить.
+ * \param char* Arg - строка с аргументом функции (Символы 'V' или 'H').
+ * \details Значение аргументов
+ * - 'V' - отражение относительно вертикальной оси.
+ * - 'H' - отражение относительно горизонтальной оси.
+ * \return void.
+ */
+void flip(Pic* Picture, char* Arg) {
+	char Dir = Arg[0];
+	// Выделение памяти для нового массива пикселей.
+	Line* ptr_linTemp;
+	ptr_linTemp = new Line[1000];
+	// Выбор
+	switch (Dir)
+	{
+	// В случае с отражением относительно горизонтальной оси, линии изначального массива пикселей записываются в новый массив в обратном порядке, 
+	// а пиксели в строках записываются в прямом порядке.
+	case 'H': {
+		for (int i = 0; i < Picture->Info.biHeight; i++) {
+			ptr_linTemp[i] = Picture->ptr_lin[Picture->Info.biHeight - 1 - i];
+		}
+		// Освобождение выделенной ранее памяти для старого массива пикселей.
+		delete[] Picture->ptr_lin;
+		break;
+	}
+	// При отражении отноительно вертикальной оси, строки из изначального массива записываются в прямом порядке, а пиксели в обратном. 
+	case 'V': {
+		for (int i = 0; i < Picture->Info.biHeight; i++) {
+			for (int j = 0; j < Picture->Info.biWidth; j++) {
+				ptr_linTemp[i].ColourIndex[j] = Picture->ptr_lin[i].ColourIndex[Picture->Info.biWidth - 1 - j];
+			}
+		}
+		// Освобождение выделенной ранее памяти для старого массива пикселей.
+		delete[] Picture->ptr_lin;
+		break;
+	}
+	default:
+		assert (false);														// Остановка программы в случае, если switch не обработал аргумент.
+	}
+	Picture->ptr_lin = ptr_linTemp;
+	return;
+}
+
+/**
+ * \brief Уменьшение количества пикселей.
+ * \details Функция уменьшает количество пикселей в V раз по вертикали и в H раз по горизонтали.
+ * \warning Операция выполняется только в том случае, если количество пикселей по вертикали и горизонтали
+ * одновременно кратны V и H соответственно.
+ * \details Уменньшение количества пикселей заключается по сути в пропуске пикселей при проходе по массиву. 
+ * Алгоритм реализуется путём прохода по изначальному массиву с шагом V по вертикали и H по горизонтали и записи пикселей в новый массив, 
+ * проход по которому в свою очередь идёт совершенно обычный.
+ * \details Функция также вносит изменения в структуры BITMAPFILEHEADER и BITMAPINFOHEADER, поскольку разрешение файла изменилось.
+ * \details Этапы работы функции:
+ * - проверка условий.
+ * - выделение памяти для нового массива пикселей.
+ * - перезапись пикселей.
+ * - изменение структуры изображения в связи с изменением разрешения.
+ * - освобождение выделенной ранее памяти для старого массива пикселей.
+ * - передача новой ссылки на массив пикселей в структуру изображения.
+ * \param Pic* Picture - указатель на структуру изображения, количество пикселей которого нужно уменьшить.
+ * \param char* Arg - строка с аргументом функции ("V H"). 1 ≤ V,H ≤ 10;
+ * \return void.
+ */
+void downscale(Pic* Picture, char* Arg) {
+	// Получение числового значения символов - аргументов (по таблице ASCII)
+	int VArg = Arg[0] - 48;		// Вертикальный делитель.
+	int HArg = Arg[2] - 48;		// Горизонтальный делитель.
+	// Проверка условий.
+	if ((Picture->Info.biHeight % VArg == 0) && (Picture->Info.biWidth % HArg == 0)) {
+		// Выделение памяти для нового массива пикселей.
+		Line* ptr_linTemp;
+		ptr_linTemp = new Line[1000];
+		// Перезапись пикселей.
+		for (int i = 0; i < Picture->Info.biHeight / VArg; i++) {
+			for (int j = 0; j < Picture->Info.biWidth / HArg; j++) {
+				// Видно, как именно реализован проход по массиву с различным шагом:
+				// Индекс пикселей в строке нового массива меняется как j, а в старом массиве как (j * Вертикальный аргумент)
+				ptr_linTemp[Picture->Info.biHeight / VArg - 1 - i].ColourIndex[j] = Picture->ptr_lin[Picture->Info.biHeight - 1 - i * VArg].ColourIndex[j * HArg];
+			}
+		}
+		// Изменение структуры изображения в связи с изменением разрешения.
+		// 1. wВысота становится шириной и наоборот
+		Picture->Info.biHeight /= VArg;
+		Picture->Info.biWidth /= HArg;
+		// 2. Рассчёт количества доп. байтов.
+		Picture->PadBytes = (4 - (Picture->Info.biWidth % 4));
+		if (Picture->PadBytes == 4)
+			Picture->PadBytes = 0;
+		// 3. Нахождения объёма массива пикселей и общего объёма занимаемого файлом (в байтах)
+		Picture->Info.biSizeImg = Picture->Info.biWidth * Picture->Info.biHeight + Picture->PadBytes * Picture->Info.biHeight;
+		Picture->File.bfSize = Picture->Info.biSizeImg + Picture->File.bfOffBit;
+		// Освобождение выделенной ранее памяти для старого массива пикселей.
+		delete[] Picture->ptr_lin;
+		// Передача новой ссылки на массив пикселей в структуру изображения.
+		Picture->ptr_lin = ptr_linTemp;
+	}
+	else {
+	#ifdef DEBUG
+		cout << "downscale command skipped" << endl;										// Контроль срабатывания команд со входными условиями.
+	#endif DEBUG
+	}
+	return;
+}
+
+/**
+ * \brief Увеличение количества пикселей.
+ * \details Функция увеличивает количество пикселей в V раз по вертикали и в H раз по горизонтали.
+ * \Warning Операция выполняется только в том случае, если при увеличении,
+ * количество пикселей по вертикали и горизонтали одновременно не привышает максимальное значение (В рамках ИЗ - 1000 пикселей)
+ * \details Алгоритм заключается в повторении каждой строки входящего изображения V раз и каждого пикселя строки H раз. 
+ * \details Функция также вносит изменения в структуры BITMAPFILEHEADER и BITMAPINFOHEADER, поскольку разрешение файла изменилось.
+ * \details Этапы работы функции:
+ * - проверка условий.
+ * - выделение памяти для нового массива пикселей.
+ * - перезапись пикселей.
+ * - изменение структуры изображения в связи с изменением разрешения.
+ * - освобождение выделенной ранее памяти для старого массива пикселей.
+ * - передача новой ссылки на массив пикселей в структуру изображения.
+ * \param Pic* Picture - указатель на структуру изображения, количество пикселей которого нужно увеличить.
+ * \param char* Arg - строка с аргументом функции ("V H"). 1 ≤ V,H ≤ 10;
+ * \return void.
+ */
+void upscale(Pic* Picture, char* Arg) {
+	// Получение числового значения символов - аргументов (по таблице ASCII)
+	int VArg = Arg[0] - 48;		// Вертикальный делитель.
+	int HArg = Arg[2] - 48;		// Горизонтальный делитель.
+	// Проверка условий.
+	if ((Picture->Info.biHeight * VArg <= 1000) && (Picture->Info.biWidth * HArg <= 1000)) {
+		// Выделение памяти для нового массива пикселей.
+		Line* ptr_linTemp;
+		ptr_linTemp = new Line[1000];
+		// Перезапись пикселей.
+		// Цикл останавливается на конкретной строке старого массива.
+		for (int i = 0; i < Picture->Info.biHeight; i++) {
+			// Запускается цикл, который будет повторять одну и ту же строку V раз. Индексы специально подобраны для удобства передачи в индексы. 
+			for (int TempV = i * VArg; TempV < (i + 1) * VArg; TempV++) {
+				// Третий цикл останавливается на конкретном пикселе строки. 
+				for (int j = 0; j < Picture->Info.biWidth; j++) {
+					// Далее запускается цикл, который будет повторять этот пиксель H раз. 
+					for (int TempH = j * HArg; TempH < (j + 1) * HArg; TempH++) {
+						// Цикл четвёртой степени вложенности может навести на мысли о долгой работе алгоритма, но в любом случае алгоритм будет заполнять матрицу не более 1000 на 1000 пикселей.
+						ptr_linTemp[TempV].ColourIndex[TempH] = Picture->ptr_lin[i].ColourIndex[j];
+					}
+				}
+			}
+		}
+		// Изменение структуры изображения в связи с изменением разрешения.
+		Picture->Info.biHeight *= VArg;
+		Picture->Info.biWidth *= HArg;
+		Picture->PadBytes = (4 - (Picture->Info.biWidth % 4));
+		if (Picture->PadBytes == 4)
+			Picture->PadBytes = 0;
+		Picture->Info.biSizeImg = Picture->Info.biWidth * Picture->Info.biHeight + Picture->PadBytes * Picture->Info.biHeight;
+		Picture->File.bfSize = Picture->Info.biSizeImg + Picture->File.bfOffBit;
+		// Освобождение выделенной ранее памяти для старого массива пикселей.
+		delete[] Picture->ptr_lin;
+		// Передача новой ссылки на массив пикселей в структуру изображения.
+		Picture->ptr_lin = ptr_linTemp;
+	}
+	else {
+	#ifdef DEBUG
+		cout << "upscale command skipped" << endl;										// Контроль срабатывания команд со входными условиями.
+	#endif DEBUG
+	}
+	return;
+}
+
+void crop(Pic* Picture, char* Arg){
+	int delta = atoi(Arg);
+	Line* ptr_linTemp;
+	ptr_linTemp = new Line[1000];
+	for (int i = delta; i < Picture->Info.biHeight - delta; i++) {
+		for (int j = delta; j < Picture->Info.biWidth - delta; j++) {
+			ptr_linTemp[i - delta].ColourIndex[j - delta] = Picture->ptr_lin[i].ColourIndex[j];
+		}
+	}
+	Picture->Info.biHeight -= 2 * delta;
+	Picture->Info.biWidth -= 2 * delta;
+	Picture->PadBytes = (4 - (Picture->Info.biWidth % 4));
+	if (Picture->PadBytes == 4)
+		Picture->PadBytes = 0;
+	Picture->Info.biSizeImg = Picture->Info.biWidth * Picture->Info.biHeight + Picture->PadBytes * Picture->Info.biHeight;
+	Picture->File.bfSize = Picture->Info.biSizeImg + Picture->File.bfOffBit;
+	delete[] Picture->ptr_lin;
+	Picture->ptr_lin = ptr_linTemp;
+	return;
+}
---
+++ inoutput.cpp
@@ -0,0 +1,227 @@
+﻿/*****************************************************************//**
+ * \file  inoutput.cpp
+ * \brief  Файл реализации функций ввода/вывода команд и изображений.
+ * \author Преучиль Н.А.
+ * \date   12 January 2021
+ *********************************************************************/
+#include "inoutput.h"
+
+/**
+ * \brief Обработка файла "command.txt".
+ * \details Функция считывает содержимое файла "command.txt", создаёт и записывает в память указатель на очередь команд,
+ * а так же количество введённых команд и изображений.
+ * \details Требования и органичения:
+ * - Количество команд не превышает 100;
+ * \details Этапы работы функции:
+ * - инициализация локальных переменных.
+ * - выделение памяти для очереди команд.
+ * - создание ссылки на файл с командами.
+ * - считывание количества картинок и команд.
+ * - считывание команд.
+ * - удаление ссылки на файл.
+ * \param InputResult* out - указатель на структуру с результатами вывода, в которую и будут сохраняться результаты
+ * работы данной подпрограммы.
+ * \return void.
+ */
+void InputCommands(InputResult* out) {
+	// Инициализация локальных переменных.
+	FILE* ptr_CommInputFile;																	
+	int CommCount;											// Количество введённых команд.
+	int PicsCount;											// Количество введённых изображений.
+	char WrongFormatPics[310];								// Строка для списка изображений, к которым будут применяться команды.
+	// Выделение памяти для очереди команд.
+	QueueElement* ptr_QueueArr;
+	ptr_QueueArr = new QueueElement[100];
+	out->ptr_QueueArr = ptr_QueueArr;						// Ссылка на очередь команд передаётся в выходную структуру out.
+	// Создание ссылки на файл с командами.
+	ptr_CommInputFile = fopen("command.txt", "rt");
+	// Считывание количества картинок и команд.
+	(void)fscanf(ptr_CommInputFile, "%d %d\n", &PicsCount, &CommCount);
+	out->PicsCount = PicsCount;								// Количество изображений передаётся в выходную структуру out.
+	out->CommCount = CommCount;								// Количество команд передаётся в выходную структуру out.
+	// Считывание команд.
+	for (int i = 0; i < CommCount; i++) {
+		(void)fscanf(ptr_CommInputFile, "%s : %s : ", &WrongFormatPics, &ptr_QueueArr[i].CommName);
+		fgets(ptr_QueueArr[i].CommArgue, 5, ptr_CommInputFile);
+		// После каждого считывания команды, вызывается функция TransformNums, преобразующая строку изображений, к которым будут применяться команды (подробнее в описании к функции TransformNums).
+		out->ptr_QueueArr[i].CommPicsCount = TransformNums(&WrongFormatPics[0], ptr_QueueArr[i].CommPics);
+	}
+	// Удаление ссылки на файл.
+	fclose(ptr_CommInputFile);
+	return;
+}
+
+/**
+ * \brief Парсер строки.
+ * \details Функция преобразует строку вида "1,2,3,4" из файла "command.txt" в массив чисел.
+ * \details Указатель передвигается по строке и пока не доходит до запятой, переносит числа в промежуточную строку. 
+ * Затем функция считывает числовое значение из строки. Далее алогритм повторяется.
+ * \details Этапы работы функции:
+ * - инициализация локальных переменных.
+ * - включение цикла, чтобы указатель передвигался, пока не встретит символ конца строки. 
+ * - 
+ * \param char* ptr_WrongStr - указатель на строку неправильного формата.
+ * \param char* ptr_ResultArr - указатель на массив чисел для заполнения.
+ * \return Функция возвращает количество элементов, записанных в массив чисел.
+ */
+int TransformNums(char* ptr_WrongStr, int* ptr_ResultArr) {
+	// Инициализация локальных переменных.
+	char* ptr_Temp;
+	ptr_Temp = &ptr_WrongStr[0];						// Указатель на входную строку.
+	char CurrentStr[5];									// Промежуточная строка.
+	int CurCounter = 0;									// Счётчик промежуточной строки.
+	int ResCounter = 0;									// Счётчик чисел в выходном массиве.
+	// Запуск цикла.
+	while (*ptr_Temp != '\0') {
+		// Условие проверяющее, не дошёл ли указатель до запятой.
+		if (*ptr_Temp != ',') {
+			// Перенос чисел в промежуточную строку.
+			CurrentStr[CurCounter] = *ptr_Temp;
+			CurCounter++;
+		}
+		else {
+			// Перенос числового значения строки в выходной массив чисел.
+			ptr_ResultArr[ResCounter] = atoi(CurrentStr);
+			ResCounter++;
+			// Очистка промежуточной строки.
+			memset(CurrentStr, 0, 4);
+			// Обнуление счётчика промежуточной строки.
+			CurCounter = 0;
+		}
+		ptr_Temp++;
+	}
+	// Обработка оставшейся строки, которая осталась, потому что строка заканчивается числом, а не запятой.
+	ptr_ResultArr[ResCounter] = atoi(CurrentStr);
+	return ResCounter + 1;
+}
+
+/**
+ * \brief Генератор имён файлов.
+ * \details Функция генерирует имена файлов для ввода/вывода и открывает файл.
+ * \details Функция имеет 2 режима работы:
+ * - 'i' - создание имени формата "inputxx.bmp";
+ * - 'o' - создание имени формата "outputxx.bmp";
+ * \details Этапы работы функции:
+ * - создание строки для имени файла.
+ * - заполнение строки.
+ * - открытие ссылки.
+ * \param int Num - Номер файла.
+ * \param char flag - символ определяющий режим работы функции.
+ * \return Функция возвращает ссылку на файл;
+ */
+FILE* FileNameGenerator(int Num, char flag) {
+	FILE* ptr_PicInputFile;
+	// Создание строки для имени файла.
+	char FileName[15];
+	if (flag == 'i') {
+		// Заполнение строки.
+		sprintf(FileName, "input%d%d.bmp", Num / 10, Num % 10);
+		// Открытие ссылки на файл входного изображения.
+		ptr_PicInputFile = fopen(FileName, "rb");
+	}
+	else if (flag == 'o') {
+		// Заполнение строки.
+		sprintf(FileName, "output%d%d.bmp", Num / 10, Num % 10);
+		// Открытие ссылки на файл выходного изображения.
+		ptr_PicInputFile = fopen(FileName, "wb");
+	}
+	else
+		ptr_PicInputFile = NULL;
+#ifdef DEBUG
+	cout << FileName << endl;							// Контроль сгенерированных имён файлов при отладке.
+#endif DEBUG
+
+	return ptr_PicInputFile;
+}
+
+/**
+ * \brief Ввод изображений.
+ * \details Функция побитово считывает изображения изображения из файлов "inputxx.bmp" и 
+ * записывает в структуру с результатами ссылку на массив с изображениями.
+ * \details Требования и органичения:
+ * - Разрешение изображений не превышает 1000 на 1000 пикселей;
+ * - Количество файлов не более 100
+ * \details Этапы работы функции:
+ * - инициализация локальных переменных.
+ * - выделение памяти для массива изображений.
+ * - запуск цикла для считывания всех изображений.
+ * - побиовое считывание каждого изображения.
+ * \param InputResult* out - указатель на структуру с результатами вывода, 
+ *  в которую и будет сохраняться ссылка на изображения.
+ * \return void.
+ */
+void InputPictures(InputResult* out) {
+	// Инициализация локальных переменных.
+	FILE* file;
+	int PicCount = out->PicsCount;									// Программа берёт количество изображений из переданной ей выходной структуры out.
+	// Выделение памяти для массива изображений.
+	Pic* ptr_Pics;
+	ptr_Pics = new Pic[100];
+	out->ptr_Pics = ptr_Pics;										// Передача ссылки на массив изображений в выходную структуру out.
+	// Запуск цикла для считывания всех изображений.
+	for (int i = 0; i < PicCount; i++) {
+		// Вызов функции FileNameGenerator для генерации ссылки на входной файл (аргумент 'i').
+		file = FileNameGenerator(i+1, 'i');
+		ptr_Pics[i].ptr_lin = new Line[1000];						// Выделение памяти для массива пикселей.
+		ptr_Pics[i].ptr_pal = new RGBQUAD[256];						// Выделение памяти для палитры.
+		// Считывание структур bmp файла.
+		fread(&ptr_Pics[i].File, sizeof(BITMAPFILEHEADER), 1, file);
+		fread(&ptr_Pics[i].Info, sizeof(BITMAPINFOHEADER), 1, file);
+		fread(ptr_Pics[i].ptr_pal, sizeof(RGBQUAD), 256, file);
+		// Вычисление количества байтов, добавленных к строкам до длины кратной 4.
+		ptr_Pics[i].PadBytes = (4 - (ptr_Pics[i].Info.biWidth % 4));
+		if (ptr_Pics[i].PadBytes == 4)
+			ptr_Pics[i].PadBytes = 0;
+		// Считывание массива пикселей.
+		for (int j = 0; j < ptr_Pics[i].Info.biHeight; j++) {
+			fread(ptr_Pics[i].ptr_lin[j].ColourIndex, 1, ptr_Pics[i].Info.biWidth, file);
+			// Программа отбрасывает пустые дополненные байты, для простоты использования команд. 
+			fseek(file, ptr_Pics[i].PadBytes, SEEK_CUR);
+		}
+	}
+	return;
+}
+
+/**
+ * \brief Полная процедура ввода данных.
+ * \details Функция введена для удобства и объединяет в себе функции ввода команд и изображений. 
+ * \details Функция создаёт, заполняет и выводит переменную типа InputResult 
+ * \return InputResult temp - структура содержащая результаты ввода данных.
+ */
+InputResult FullInputFunc() {
+	// Инициализация выходной структуры.
+	InputResult temp;
+	InputCommands(&temp);
+	InputPictures(&temp);
+	return temp;
+}
+
+/**
+ * \brief Вывод изображений.
+ * \details Функция побитово выводит изображения из массива в файлы "outputxx.bmp".
+ * \details Этапы работы функции:
+ * - функция инициализирует файловую переменную.
+ * - запуск цикла для вывода всех изображений.
+ * \param int PicCount - Количество изображений, которые необходимо вывести.
+ * \param Pic* ptr_Pics - Массив изображений.
+ * \return void.
+ */
+void OutputFunc(int PicCount, Pic* ptr_Pics) {
+	// Функция инициализирует файловую переменную.
+	FILE* file;
+	// Запуск цикла для вывода всех изображений.
+	for (int i = 0; i < PicCount; i++) {
+		// Вызов функции FileNameGenerator для генерации ссылки на выходной файл (аргумент 'o').
+		file = FileNameGenerator(i + 1, 'o');
+		// Вывод структур файла и массива пикселей.
+		fwrite(&ptr_Pics[i].File, sizeof(BITMAPFILEHEADER), 1, file);
+		fwrite(&ptr_Pics[i].Info, sizeof(BITMAPINFOHEADER), 1, file);
+		fwrite(ptr_Pics[i].ptr_pal, sizeof(RGBQUAD), 256, file);
+		for (int j = 0; j < ptr_Pics[0].Info.biHeight; j++) {
+			fwrite(ptr_Pics[i].ptr_lin[j].ColourIndex, 1, ptr_Pics[i].Info.biWidth, file);
+			// Добавление байтов к строкам до кратности 4-м.
+			fwrite(&ZeroBite, 1, ptr_Pics[0].PadBytes, file);
+		}
+	}
+	return;
+}
